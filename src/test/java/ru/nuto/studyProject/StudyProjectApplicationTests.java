2e5592ff5b74a94f51cae77bf0e956eae0d9aa29
nuto17 <nutolfg@gmail.com>
2025-02-08 17:29:09 +0300

    first

---
+++ src/test/java/ru/nuto/studyProject/StudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nuto.studyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
