d420d7143fbc928c939ab74cc540b739ada38815
nuto17 <nutolfg@gmail.com>
2025-02-10 22:02:48 +0300

    new crud-user branch

--- src/main/java/ru/nuto/studyproject/exception/NotFoundTaskException.java
+++
@@ -1,7 +0,0 @@
-package ru.nuto.studyproject.exception;
-
-public class NotFoundTaskException extends Throwable {
-    public NotFoundTaskException(Long id) {
-        super(String.format("задачи с id: %d\n не найдено", id));
-    }
-}
